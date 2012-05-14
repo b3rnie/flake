cf9fa8291d1e38efab2dbb4a998d06a4a8d63ca5
Bjorn Jensen-Urstad <bjorn.urstad@klarna.com>
2012-05-14 16:47:05 +0200

    Refactored it slightly

--- src/flake_server.erl
+++ src/flake_server.erl
@@ -13,95 +13,90 @@
 %%% See the License for the specific language governing permissions and
 %%% limitations under the License.
 %%%
-
--module (flake_server).
 -author ('Dietrich Featherston <d@boundary.com>').
+%%% Refactored 2012 / Bjorn Jensen-Urstar <bjorn.urstad@klarna.com>
 
--behaviour (gen_server).
+%%%_* Module declaration ===============================================
+-module(flake_server).
+-behaviour(gen_server).
 
-%% API
--export ([
-	  start_link/1,
-	  id/0,
-	  id/1
-	 ]).
+%%%_* Exports ==========================================================
+-export([ start_link/1
+        , id/0
+        , id/1
+        ]).
 
-%% gen_server callbacks
--export ([
-	  init/1,
-	  handle_call/3,
-	  handle_cast/2,
-	  handle_info/2,
-	  terminate/2,
-	  code_change/3
-	 ]).
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
+        ]).
 
--include_lib ("eunit/include/eunit.hrl").
+%%%_* Code =============================================================
+%%%_ * Types -----------------------------------------------------------
+-record(s, { tref
+           , max_time
+           , worker_id
+           , sequence :: integer()
+           }).
+%%%_ * API -------------------------------------------------------------
+start_link(Args) ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
 
--record (state, {max_time, worker_id, sequence}).
+%% @doc generate a new snowflake id
+id()     -> gen_server:call(?MODULE, get).
+id(Base) -> gen_server:call(?MODULE, {get, Base}).
 
-
-%% ----------------------------------------------------------
-%% API
-%% ----------------------------------------------------------
-
-% start and link to a new flake id generator
-start_link(Config) ->
-    gen_server:start_link({local,flake}, ?MODULE, Config, []).
-
-% generate a new snowflake id
-id() ->
-    respond(gen_server:call(flake, get)).
-
-id(Base) ->
-    respond(gen_server:call(flake, {get, Base})).
-
-respond({ok,Flake}) ->
-    {ok,Flake};
-respond(X) ->
-    X.
-
-
-%% ----------------------------------------------------------
-%% gen_server callbacks
-%% ----------------------------------------------------------
-
+%%%_ * gen_server callbacks --------------------------------------------
 init([{worker_id, WorkerId}]) ->
-    {ok, #state{max_time=flake_util:curr_time_millis(), worker_id=WorkerId, sequence=0}}.
+  {ok, #s{ max_time=flake_util:curr_time_millis()
+         , worker_id=WorkerId
+         , sequence=0}}.
 
-handle_call(get, _From, State = #state{max_time=MaxTime, worker_id=WorkerId, sequence=Sequence}) ->
-    {Resp, S0} = get(flake_util:curr_time_millis(), MaxTime, WorkerId, Sequence, State),
-    {reply, Resp, S0};
+handle_call(get, _From, #s{ max_time = MaxTime
+                          , worker_id = WorkerId
+                          , sequence = Sequence
+                          } = S0) ->
+    {Resp, S1} = get(flake_util:curr_time_millis(), MaxTime, WorkerId,
+                     Sequence, S0),
+    {reply, Resp, S1};
 
-handle_call({get,Base}, _From, State = #state{max_time=MaxTime,worker_id=WorkerId,sequence=Sequence}) ->
-    {Resp, S0} = get(flake_util:curr_time_millis(), MaxTime, WorkerId, Sequence, State),
+
+handle_call({get, Base}, _From, #s{ max_time = MaxTime
+                                  , worker_id = WorkerId
+                                  , sequence  = Sequence
+                                  } = S0) ->
+    {Resp, S1} = get(flake_util:curr_time_millis(), MaxTime, WorkerId,
+                     Sequence, S0),
     case Resp of
-	{ok, Id} ->
-	    <<IntId:128/integer>> = Id,
+	{ok, <<IntId:128/integer>>} ->
 	    {reply, {ok, flake_util:as_list(IntId, Base)}, S0};
 	E ->
 	    {reply, E, S0}
-    end;
-  
-handle_call(X, _From, State) ->
-  error_logger:error_msg("unrecognized msg in ~p:handle_call -> ~p~n",[?MODULE, X]),
-  {reply, ok, State}.
+    end.
 
-handle_cast(_, State) -> {noreply, State}.
+handle_cast(_Msg, S) -> {noreply, S}.
 
-handle_info(_, State) -> {noreply, State}.
+handle_info(_Info, S) -> {noreply, S}.
 
-terminate(_Reason, _State) -> ok.
+terminate(_Rsn, #s{tref = TRef}) ->
+    timer:cancel(TRef),
+    ok.
 
-code_change(_, State, _) -> {ok, State}.
-  
+code_change(_OldVsn, S, _Extra) ->
+    {ok, S}.
+
+%%%_ * Internals -------------------------------------------------------
+
 %% clock hasn't moved, increment sequence
-get(Time,Time,WorkerId,Seq0,State) ->
+get(Time,Time,WorkerId,Seq0,S) ->
     Sequence = Seq0 + 1,
-    {{ok,flake_util:gen_id(Time,WorkerId,Sequence)},State#state{sequence=Sequence}};
+    {{ok,flake_util:gen_id(Time,WorkerId,Sequence)},S#s{sequence=Sequence}};
 %% clock has progressed, reset sequence
-get(CurrTime,MaxTime,WorkerId,_,State) when CurrTime > MaxTime ->
-  {{ok, flake_util:gen_id(CurrTime, WorkerId, 0)}, State#state{max_time=CurrTime, sequence=0}};
+get(CurrTime,MaxTime,WorkerId,_,S) when CurrTime > MaxTime ->
+  {{ok, flake_util:gen_id(CurrTime, WorkerId, 0)}, S#s{max_time=CurrTime, sequence=0}};
 %% clock is running backwards
-get(CurrTime, MaxTime, _WorkerId, _Sequence, State) when MaxTime > CurrTime ->
-  {{error, clock_running_backwards}, State}.
+get(CurrTime, MaxTime, _WorkerId, _Sequence, S) when MaxTime > CurrTime ->
+  {{error, clock_running_backwards}, S}.

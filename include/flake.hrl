6e63856aadc20c131f9d6c997e207caef27eb23e
Bjorn Jensen-Urstad <bjorn.jensen.urstad@gmail.com>
2012-09-13 14:43:56 +0200

    Forgot

---
+++ include/flake.hrl
@@ -0,0 +1,9 @@
+-ifndef(__FLAKE_HRL__).
+-define(__FLAKE_HRL__, false).
+
+-define(interface,          any).
+-define(interval,           2000).
+-define(timestamp_path,     "/tmp").
+-define(allowable_downtime, 2592000000).
+
+-endif.
